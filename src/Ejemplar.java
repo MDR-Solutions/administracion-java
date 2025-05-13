4e589042a38071f770425dc091f4f3fe5ae65dcd
CrisGreenfield <cristobal.greenfield03@somo.eus>
2025-05-13 12:33:08 +0200

    Reorganizar funciones ajenas a Io

--- src/Ejemplar.java
+++ src/Ejemplar.java
@@ -1,24 +1,31 @@
+import java.sql.*;
+
 public class Ejemplar {
-    //Atributos
-	private int id_ejemplar;
+    // Atributos
+    private int id_ejemplar;
     private String estado_fisico;
     private int id_libro;
+
     //Constructor
     public Ejemplar(int id, String estado, int libro) {
         this.id_ejemplar = id;
         this.estado_fisico = estado;
         this.id_libro = libro;
     }
+    
     //Getters y Setters
     public int getIdEjemplar() {
         return id_ejemplar;
     }
+
     public void setIdEjemplar(int id) {
         this.id_ejemplar = id;
     }
+
     public String getEstadoFisico() {
         return estado_fisico;
     }
+
     public void setEstadoFisico(String estado) {
         this.estado_fisico = estado;
     }
@@ -26,7 +33,149 @@ public class Ejemplar {
     public int getIdLibro() {
         return id_libro;
     }
+
     public void setIdLibro(int libro) {
         this.id_libro = libro;
+    }
+
+    // Menu de ejemplar
+    public static void menuEjemplar(Connection conn) {
+        Scanner input = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+            System.out.println("Menu de ejemplar:");
+            System.out.println("1. Ver lista de ejemplares");
+            System.out.println("2. Añadir un ejemplar");
+            System.out.println("3. Eliminar un ejemplar");
+            System.out.println("4. Salir");
+            System.out.print("Selecciona una opción: ");
+            opcion = Io.leerNumero();
+
+            switch (opcion) {
+                case 1:
+                    try {
+                        ejecutarSelectEjemplar(conn);
+                    } catch (SQLException e) {
+                        System.out.println("Error al mostrar ejemplares: " + e.getMessage());
+                    }
+                    break;
+                case 2:
+                    try {
+                        crearEjemplar(conn);
+                    } catch (SQLException | InterruptedException e) {
+                        System.out.println("Error al crear ejemplar: " + e.getMessage());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Introduce el ID del ejemplar a eliminar: ");
+                    int id_ejemplar = Io.leerNumero();
+                    Ejemplar ejemplarAEliminar = new Ejemplar(id_ejemplar, null, 0);
+                    try {
+                        boolean eliminado = ejecutarDeleteEjemplar(conn, ejemplarAEliminar);
+                        if (eliminado) {
+                            System.out.println("Ejemplar eliminado correctamente.");
+                        } else {
+                            System.out.println("No se pudo eliminar el ejemplar.");
+                        }
+                    } catch (SQLException e) {
+                        System.out.println("Error al eliminar ejemplar: " + e.getMessage());
+                    }
+                    break;
+                case 4:
+                    System.out.println("Saliendo del menú de ejemplar...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+            }
+        } while (opcion != 4);
+    }
+
+    // Funciones de Ejemplar
+    public static int ejecutarInsertEjemplar(Connection conn, Ejemplar obj) throws SQLException {
+        int id_ejemplar = obj.getIdEjemplar();
+        String estado_fisico = obj.getEstadoFisico();
+        int id_libro = obj.getIdLibro();
+
+        String str = "INSERT INTO TEJEMPLARES (EJECOD, EJEFISICO, LIBID) VALUES ('" + id_ejemplar + "', '" + estado_fisico + "', '" + id_libro + "')";
+
+        Statement stmt = conn.createStatement();
+        int numeroCambios = stmt.executeUpdate(str);
+
+        return numeroCambios;
+    }
+
+    public static void ejecutarSelectEjemplar(Connection conn) throws SQLException {
+        String sql = "SELECT * FROM TEJEMPLARES";
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                int id_ejemplar = rs.getInt("EJECOD");
+                String estado_fisico = rs.getString("EJEFISICO");
+                int id_libro = rs.getInt("LIBID");
+
+                System.out.println("------------------------------------------------------------------------------");
+                System.out.println("ID Ejemplar: " + id_ejemplar + ", Estado Fisico: " + estado_fisico + ", ID Libro: " + id_libro);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static boolean ejecutarDeleteEjemplar(Connection conn, Ejemplar obj) throws SQLException {
+        int id_ejemplar = obj.getIdEjemplar();
+        String sql = "DELETE FROM TEJEMPLARES WHERE EJECOD = '" + id_ejemplar + "'";
+
+        Statement stmt = conn.createStatement();
+        return stmt.executeUpdate(sql) > 0;
+    }
+
+    public static Ejemplar crearEjemplar(Connection conn) throws SQLException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce el ID del ejemplar: ");
+        int id_ejemplar = Io.leerNumero();
+        System.out.println("Introduce el estado fisico: ");
+        String estado_fisico = input.next();
+        System.out.println("Introduce el ID del libro: ");
+        int id_libro = Io.leerNumero();
+
+        Ejemplar nuevoEjemplar = new Ejemplar(id_ejemplar, estado_fisico, id_libro);
+        int cambios = ejecutarInsertEjemplar(conn, nuevoEjemplar);
+
+        if (cambios == 0) {
+            System.out.println("No se ha podido añadir el registro");
+        } else {
+            System.out.println("Registro añadido correctamente");
+        }
+        return nuevoEjemplar;
+    }
+
+    public static Ejemplar BuscarEjemplar(Connection conn, int id_ejemplar) throws SQLException {
+        String sql = "SELECT * FROM TEJEMPLARES WHERE EJECOD = '" + id_ejemplar + "'";
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+
+            if (rs.next()) {
+                int swid_ejemplar = rs.getInt("EJECOD");
+                String swestado_fisico = rs.getString("EJEFISICO");
+
+                Ejemplar libro = new Ejemplar(swid_ejemplar, swestado_fisico, 0);
+                System.out.println("------------------------------------------------------------------------------");
+                System.out.println("ID Libro: 0, Estado Fisico: " + swestado_fisico + ", ID Ejemplar: " + swid_ejemplar);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null; // No devuelve el libro
     }
 }
--- src/Prestamo.java
+++ src/Prestamo.java
@@ -1,48 +1,153 @@
-import java.sql.Date;
-
 public class Prestamo {
-		//Atributos
-	 	private int id_prestamo;
-	    private String dni;
-	    private int id_ejemplar;
-	    private Date fecha_prestamo, fecha_devolucion;
-	    //Constructor
-	    public Prestamo(int id, String d, int ej, String fp, String fd) {
-	        this.id_prestamo = id;
-	        this.dni = d;
-	        this.id_ejemplar = ej;
-			this.fecha_prestamo = Date.valueOf(fp);
-			this.fecha_devolucion = Date.valueOf(fd);
-		}
-		//Getters y Setters
-		public int getId_prestamo() {
-			return id_prestamo;
-		}
-		public void setId_prestamo(int id_prestamo) {
-			this.id_prestamo = id_prestamo;
-		}
-		public String getDni() {
-			return dni;
-		}
-		public void setDni(String dni) {
-			this.dni = dni;
-		}
-		public int getId_ejemplar() {
-			return id_ejemplar;
-		}
-		public void setId_ejemplar(int id_ejemplar) {
-			this.id_ejemplar = id_ejemplar;
-		}
-		public Date getFecha_prestamo() {
-			return fecha_prestamo;
-		}
-		public void setFecha_prestamo(Date fecha_prestamo) {
-			this.fecha_prestamo = fecha_prestamo;
-		}
-		public Date getFecha_devolucion() {
-			return fecha_devolucion;
-		}
-		public void setFecha_devolucion(Date fecha_devolucion) {
-			this.fecha_devolucion = fecha_devolucion;
-	    }
+	//Atributos
+    private int id_prestamo;
+    private String dni;
+    private int id_ejemplar;
+    private java.time.LocalDate fecha_prestamo, fecha_devolucion;
+
+    // Constructor
+    public Prestamo(int id, String d, int ej, String fp, String fd) {
+        this.id_prestamo = id;
+        this.dni = d;
+        this.id_ejemplar = ej;
+        this.fecha_prestamo = java.time.LocalDate.parse(fp);
+        this.fecha_devolucion = java.time.LocalDate.parse(fd).plusDays(15);
+    }
+
+    // Getters y Setters
+    public int getId_prestamo() {
+        return id_prestamo;
+    }
+
+    public void setId_prestamo(int id) {
+        this.id_prestamo = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getId_ejemplar() {
+        return id_ejemplar;
+    }
+
+    public void setId_ejemplar(int id_ejemplar) {
+        this.id_ejemplar = id_ejemplar;
+    }
+
+    public java.time.LocalDate getFecha_prestamo() {
+        return fecha_prestamo;
+    }
+
+    public void setFecha_prestamo(java.time.LocalDate fecha_prestamo) {
+        this.fecha_prestamo = fecha_prestamo;
+    }
+
+    public java.time.LocalDate getFecha_devolucion() {
+        return fecha_devolucion;
+    }
+
+    public void setFecha_devolucion(java.time.LocalDate fecha_devolucion) {
+        this.fecha_devolucion = fecha_devolucion;
+    }
+
+    // Funciones de Prestamo
+    public static int ejecutarInsertPrestamo(Connection conn, Prestamo obj) throws SQLException {
+        int id_prestamo = obj.getId_prestamo();
+        String dni = obj.getDni();
+        int id_ejemplar = obj.getId_ejemplar();
+        java.time.LocalDate fecha_prestamo = obj.getFecha_prestamo();
+        java.time.LocalDate fecha_devolucion = obj.getFecha_devolucion();
+
+        String str = "INSERT INTO TPRESTAMOS (PREID, PREUSUDNI, PREEJECOD, PREFECHAPRESTAMO, PREFECHADEVOLUCION) VALUES ('" + id_prestamo + "', '" + dni + "', '" + id_ejemplar + "', '" + fecha_prestamo + "', '" + fecha_devolucion + "')";
+
+        Statement stmt = conn.createStatement();
+        int numeroCambios = stmt.executeUpdate(str);
+
+        return numeroCambios;
+    }
+
+    public static void ejecutarSelectPrestamo(Connection conn) throws SQLException {
+        String sql = "SELECT * FROM TPRESTAMOS";
+        ResultSet rs = null;
+        Statement st = null;
+
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                int id_prestamo = rs.getInt("PREID");
+                String dni = rs.getString("PREUSUDNI");
+                int id_ejemplar = rs.getInt("PREEJECOD");
+                java.time.LocalDate fecha_prestamo = rs.getObject("PREFECHAPRESTAMO", java.time.LocalDate.class);
+                java.time.LocalDate fecha_devolucion = rs.getObject("PREFECHADEVOLUCION", java.time.LocalDate.class);
+
+                System.out.println("------------------------------------------------------------------------------");
+                System.out.println("ID Prestamo: " + id_prestamo + ", DNI: " + dni + ", ID Ejemplar: " + id_ejemplar + ", Fecha Prestamo: " + fecha_prestamo + ", Fecha Devolucion: " + fecha_devolucion);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static boolean ejecutarDeletePrestamo(Connection conn, Prestamo obj) throws SQLException {
+        int id_prestamo = obj.getId_prestamo();
+        String sql = "DELETE FROM TPRESTAMOS WHERE PREID = '" + id_prestamo + "'";
+
+        Statement stmt = conn.createStatement();
+        return stmt.executeUpdate(sql) > 0;
+    }
+
+    public static Prestamo crearPrestamo(Connection conn) throws SQLException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce el ID del prestamo: ");
+        int id_prestamo = Io.leerNumero();
+        System.out.println("Introduce el DNI: ");
+        String dni = input.next();
+        System.out.println("Introduce el ID del ejemplar: ");
+        int id_ejemplar = Io.leerNumero();
+
+        java.time.LocalDate fechaPrestamo = java.time.LocalDate.parse(Io.leerFecha());
+        java.time.LocalDate fechaDevolucion = fechaPrestamo.plusDays(15);
+
+        Prestamo nuevoPrestamo = new Prestamo(id_prestamo, dni, id_ejemplar, fechaPrestamo.toString(), fechaDevolucion.toString());
+        int cambios = ejecutarInsertPrestamo(conn, nuevoPrestamo);
+
+        if (cambios == 0) {
+            System.out.println("No se ha podido añadir el registro");
+        } else {
+            System.out.println("Registro añadido correctamente");
+        }
+        input.close();
+        return nuevoPrestamo;
+    }
+
+    public static Prestamo BuscarPrestamo(Connection conn, int id_prestamo) throws SQLException {
+        String sql = "SELECT * FROM TPRESTAMOS WHERE PREID = '" + id_prestamo + "'";
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+
+            if (rs.next()) {
+                int swid_prestamo = rs.getInt("PREID");
+                String swdni = rs.getString("PREUSUDNI");
+
+                Prestamo libro = new Prestamo(swid_prestamo, swdni, 0, "", "");
+                System.out.println("------------------------------------------------------------------------------");
+                System.out.println("ID Libro: 0, DNI: " + swdni + ", ID Ejemplar: " + swid_prestamo);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null; // No devuelve el libro
+    }
 }
